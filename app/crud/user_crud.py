c0ac6b0d1a82cfc69f58ca8e66c6bddc6ec3ee20
mabuujin <ruslanyu71@gmail.com>
2026-07-18 22:07:46 +0300

    Refractoring for tests

--- app/crud/user_crud.py
+++ app/crud/user_crud.py
@@ -1,76 +1,88 @@
-from app.database import get_session
-from app.schemas import UserCreate, UserEdit, UserPublic
-from app.models import User, UserRole
-from app.utilities import oauth2_scheme, get_access_token, authenticate_user, validate_token
 from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 import bcrypt
 from typing import Annotated
 from sqlalchemy import select
+from sqlalchemy.orm import Session
+import os
+from dotenv import load_dotenv
 
+from app.database import get_session
+from app.schemas import UserCreate, UserEdit, UserPublic
+from app.models import User, UserRole
+from app.utilities import oauth2_scheme, get_access_token, authenticate_user, validate_token
 
-def sign_up(data: UserCreate):
-    with get_session() as session:
-        if data.role == UserRole.admin:
-            raise HTTPException(status_code=403, detail="You are Forbidden from performing this operation")
+load_dotenv()
+BCRYPT_ROUNDS = os.getenv("BCRYPT_ROUNDS")
 
-        s = bcrypt.gensalt()
-        pw = data.password.encode("utf-8")
-        pw_hash = bcrypt.hashpw(pw, s)
-        pw_db = pw_hash.decode("utf-8")
-        
-        user = User(
-            name = data.name,
-            password_hash = pw_db,
-            email = data.email,
-            role = data.role
-        )
-
-        session.add(user)
-        session.commit()
+def sign_up(data: UserCreate, session: Session):
+    if data.role == UserRole.admin:
+        raise HTTPException(status_code=403, detail="You are Forbidden from performing this operation")
     
-def sing_in(data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    with get_session() as session:
-        stmt = select(User).where(User.name == data.username)
-        user = session.scalar(stmt)
-        if not authenticate_user(data, user):
-            raise HTTPException(status_code=401, detail="Invalid username or password")
-        
-        token = get_access_token(user)
-        return token
+    stmt = select(User).where(User.name == data.name)
+    check_user = session.scalar(stmt)
+    if check_user:
+        raise HTTPException(status_code=403, detail="User already exists")
+
+    s = bcrypt.gensalt(rounds=int(BCRYPT_ROUNDS))
+    pw = data.password.encode("utf-8")
+    pw_hash = bcrypt.hashpw(pw, s)
+    pw_db = pw_hash.decode("utf-8")
     
-def edit_user(token: Annotated[str, Depends(oauth2_scheme)], edit_body: UserEdit, id: int):
-    with get_session() as session:
-        payload = validate_token(token)
-        user = session.get(User, id)
-        if payload.get("sub") != user.name:
-            raise HTTPException(status_code=403, detail="You are forbidden from performing this operation")
-        
-        edit_user = edit_body.model_dump(exclude_unset=True)
-        for key, value, in edit_user.items():
-            setattr(user, key, value)
+    user = User(
+        name = data.name,
+        password_hash = pw_db,
+        email = data.email,
+        role = data.role
+    )
 
-        session.commit()
+    session.add(user)
+    session.commit()
+    return user
+    
+def sing_in(data: Annotated[OAuth2PasswordRequestForm, Depends()], session: Session):
+    stmt = select(User).where(User.name == data.username)
+    user = session.scalar(stmt)
+    if not authenticate_user(data, user):
+        raise HTTPException(status_code=401, detail="Invalid username or password")
+    
+    token = get_access_token(user)
+    return token
+    
+def edit_user(token: Annotated[str, Depends(oauth2_scheme)], edit_body: UserEdit, session: Session):
+    payload = validate_token(token)
+    id = edit_body.id
+    user = session.get(User, id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if payload.get("sub") != user.name:
+        raise HTTPException(status_code=403, detail="You are forbidden from performing this operation")
+    
+    edit_user = edit_body.model_dump(exclude_unset=True)
+    for key, value, in edit_user.items():
+        setattr(user, key, value)
 
-def delete_user(token: Annotated[str, Depends(oauth2_scheme)], id: int):
-    with get_session() as session:
-        payload = validate_token(token)
-        user = session.get(User, id)
-        if payload.get("sub") != user.name:
-            raise HTTPException(status_code=403, detail="You are forbidden from performing this operation")
-        
-        session.delete(user)
-        session.commit()
+    session.commit()
 
-def get_user(id: int):
-    with get_session() as session:
-        user = session.get(User, id)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
-        user_display = UserPublic(
-            id = user.id,
-            name = user.name,
-            email = user.email,
-            role = user.role
-        )
+def delete_user(token: Annotated[str, Depends(oauth2_scheme)], id: int, session: Session):
+    payload = validate_token(token)
+    user = session.get(User, id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if payload.get("sub") != user.name:
+        raise HTTPException(status_code=403, detail="You are forbidden from performing this operation")
+    
+    session.delete(user)
+    session.commit()
+
+def get_user(id: int, session: Session):
+    user = session.get(User, id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    user_display = UserPublic(
+        id = user.id,
+        name = user.name,
+        email = user.email,
+        role = user.role
+    )
     return user_display
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,37 +1,40 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
+from typing import Annotated
+from sqlalchemy.orm import Session
+
 from app.crud import sign_up, sing_in, edit_user, delete_user, get_user
 from app.schemas import UserCreate, UserEdit
-from typing import Annotated
 from app.utilities import oauth2_scheme
-
+from app.database import get_session
+ 
 router = APIRouter(
     prefix="/users",
     tags=["users"]
 )
 
 @router.post("/sign-up")
-async def sign_up_r(data: UserCreate):
-    sign_up(data)
+async def sign_up_r(data: UserCreate, session: Annotated[Session, Depends(get_session)]):
+    sign_up(data, session)
     return JSONResponse(status_code=201, content={"message": "User created successfully"})
 
 @router.post("/sign-in")
-async def sign_in_r(data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    token = sing_in(data)
+async def sign_in_r(data: Annotated[OAuth2PasswordRequestForm, Depends()], session: Annotated[Session, Depends(get_session)]):
+    token = sing_in(data, session)
     return token
 
 @router.patch("/edit-user")
-async def edit_user_r(token: Annotated[str, Depends(oauth2_scheme)], edit_body: UserEdit, id: int):
-    edit_user(token, edit_body, id)
+async def edit_user_r(token: Annotated[str, Depends(oauth2_scheme)], edit_body: UserEdit, session: Annotated[Session, Depends(get_session)]):
+    edit_user(token, edit_body, session)
     return JSONResponse(status_code=200, content={"message": "User edited successfully"})
 
 @router.delete("/delete-user")
-async def delete_user_r(token: Annotated[str, Depends(oauth2_scheme)], id: int):
-    delete_user(token, id)
-    return JSONResponse(status_code=200, content={"message": "User deleted succesfully"})
+async def delete_user_r(token: Annotated[str, Depends(oauth2_scheme)], id: int,  session: Annotated[Session, Depends(get_session)]):
+    delete_user(token, id, session)
+    return JSONResponse(status_code=200, content={"message": "User deleted successfully"})
 
 @router.get("/{id}")
-def get_user_r(id: int):
-    user = get_user(id)
+def get_user_r(id: int, session: Annotated[Session, Depends(get_session)]):
+    user = get_user(id, session)
     return user
